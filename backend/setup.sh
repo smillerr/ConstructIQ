61095fad57cee15259c817d7823b1f891ef2e7d8
Gabriel Arango <gabrielarango15@gmail.com>
2024-04-11 19:49:35 -0500

    Implement auth for superuser, add docker files

---
+++ backend/setup.sh
@@ -0,0 +1,4 @@
+python manage.py makemigrations
+python manage.py migrate
+# python manage.py createsuperuser --email test@example.org --username admin --no-input
+python manage.py runserver 0.0.0.0:8000
--- backend/user_management/views.py
+++ backend/user_management/views.py
@@ -1,9 +1,20 @@
 from rest_framework import viewsets
 from .serializer import UsuarioSerializer
 from .models import Usuario
+from rest_framework.permissions import IsAuthenticated  
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 
 # Create your views here.
 
+#Añadir decoradores para proteger acceso
+
+
+class LoginView(TokenObtainPairView):
+    pass
+
 class UsuarioViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
