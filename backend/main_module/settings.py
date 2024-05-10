20a65de3eadcd6ec24262845684925d29e262c6f
Gabriel Arango <gabrielarango15@gmail.com>
2024-05-10 04:30:07 -0500

    Feat (backend): Add refresh token rotation setting, set default access token lifetime

--- backend/main_module/settings.py
+++ backend/main_module/settings.py
@@ -142,8 +142,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1440),
+#    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1440),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True
 }
 
 REST_FRAMEWORK = {
