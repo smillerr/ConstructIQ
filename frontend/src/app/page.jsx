9939d6ce5af232b72eaf31a2164595ac845d1773
Sebastian Miller <smillerr33@gmail.com>
2024-05-26 17:14:09 -0500

    feat(frontend): redirect user from login page to dashboard page when already logged

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,6 +1,15 @@
+import { redirect } from 'next/navigation'
+import { getSession } from './lib/utils/auth'
 import UserLoginForm from './ui/user/UserLoginForm'
+import { dashboardPaths } from './lib/utils/utilFunctions'
 
-export default function Home() {
+export default async function Home() {
+  const session = await getSession()
+
+  if (session) {
+    const userType = await session.user.tipo_usuario
+    redirect(dashboardPaths(userType))
+  }
   return (
     <section className="bg-white ">
       <div className="container flex items-center justify-center min-h-screen px-6 mx-auto">
