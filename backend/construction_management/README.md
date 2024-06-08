48f1d1d619e250b0740884aa88ba974bf75e51d7
nomka <juanjose111102@gmail.com>
2024-06-07 22:05:45 -0500

    [added] new entry to construct changelog

--- backend/construction_management/README.md
+++ backend/construction_management/README.md
@@ -1,4 +1,7 @@
 # CHANGELOG
 
+## june 07. 22:05
+
 * this changelog was added 
 * ```obras-personal``` added to views and url, so now you can make petitions to the **API** 
+
--- backend/construction_management/urls.py
+++ backend/construction_management/urls.py
@@ -4,6 +4,7 @@ from construction_management import views
 
 router=routers.DefaultRouter()
 router.register(r'obras', views.ObraViewSet)
+router.register(r'obras-personal', views.ObraPersonalViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
