8fed221257e7c3d623db26b76bf7ca67baee2ae4
nomka <juanjose111102@gmail.com>
2024-06-09 04:20:53 -0500

    [modified] obras and obrapersonal serializers now working together (kinda)

--- backend/construction_management/README.md
+++ backend/construction_management/README.md
@@ -5,3 +5,9 @@
 * this changelog was added 
 * ```obras-personal``` added to views and url, so now you can make petitions to the **API** 
 * serializer file changed, added serializers for usuarios and obrapersonal 
+
+## june 08 
+
+* obras and obrapersonal serializers now working together (kinda)
+
+
--- backend/construction_management/serializer.py
+++ backend/construction_management/serializer.py
@@ -9,11 +9,13 @@ class UsuarioSerializer(serializers.ModelSerializer):
         #exclude = ['password']
 
 class ObraPersonalSerializer(serializers.ModelSerializer):
-    #personal = UsuarioSerializer(many=True, read_only=True)
+    #personal = UsuarioSerializer(many=True)
     class Meta:
         model = ObraPersonal
-        fields = '__all__'
+        #fields = '__all__'
         #exclude = ['id_obra']
+        fields = ['personal']
+
 
 #para incluir datos en el obra serializer
 class IncludePersonalSerializer(serializers.ModelSerializer):
@@ -25,23 +27,63 @@ class IncludePersonalSerializer(serializers.ModelSerializer):
 
 
 class ObraSerializer(serializers.ModelSerializer):
-    obra_personal = IncludePersonalSerializer(source='obrapersonal', read_only=True)
+    obra_personal = ObraPersonalSerializer(source='obrapersonal', read_only=False)
+    obra_personal_read = IncludePersonalSerializer(source='obrapersonal', read_only=True)
     id_capataces = serializers.PrimaryKeyRelatedField(queryset=Usuario.objects.all(), many=True, required=False)
     id_director = serializers.PrimaryKeyRelatedField(queryset=Usuario.objects.all(), required=False)
+
     class Meta:
         model=Obra
-        fields = '__all__'
+        #fields = '__all__'
+        fields = ['id', 'obra_personal', 'obra_personal_read', 'id_capataces', 'id_director', 'nombre', 'descripcion', 'ubicacion', 'estado', 'activo', 'tipo_obra', 'fecha_inicio', 'fecha_final']
+        extra_kwargs = {
+            'obra_personal_read': {'read_only': True},
+        }
 
-""" 
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['id_capataces'] = UsuarioSerializer(instance.id_capataces, many=True).data
+        representation['id_director'] = UsuarioSerializer(instance.id_director).data
+        return representation
+
+
     def create(self, validated_data):
-        obra_personal_data = validated_data.pop('obra_personal')
+        id_capataces = validated_data.pop('id_capataces', None)
+        obra_personal_data = validated_data.pop('obrapersonal', None)
 
         obra = Obra.objects.create(**validated_data)
 
-        id_obra = obra.id
+        if id_capataces:
+            obra.id_capataces.set(id_capataces)
 
-        # asigna el id de obra en obra a obra personal, para no volverlo a poner
-        ObraPersonal.objects.create(id_obra=obra, **obra_personal_data)
+        # obra_personal = ObraPersonal.objects.create(id_obra=obra)
+
+        if obra_personal_data:
+            personal = obra_personal_data.get('personal')
+            obra_personal = ObraPersonal.objects.create(id_obra=obra)
+
+            if personal:
+                obra_personal.personal.set(personal)
+            obra_personal.save()
 
         return obra
-"""
+
+    def update(self, instance, validated_data):
+        id_capataces = validated_data.pop('id_capataces', None)
+        obra_personal_data = validated_data.pop('obrapersonal', None)
+
+        instance = super().update(instance, validated_data)
+
+        if id_capataces:
+            instance.id_capataces.set(id_capataces)
+
+        if obra_personal_data:
+            if hasattr(instance, 'obrapersonal'):
+                obra_personal = instance.obrapersonal
+            else:
+                obra_personal = ObraPersonal.objects.create(id_obra=instance)
+            obra_personal.personal.set(obra_personal_data.get('personal', []))
+            obra_personal.save()
+
+        return instance
